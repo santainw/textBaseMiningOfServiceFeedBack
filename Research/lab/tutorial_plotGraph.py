7ca12c3e1201f54ab82b6f0f0a54eefbc99524d8
Bloodblack <kaimaimoo123@gmail.com>
2018-09-08 16:00:08 +0700

    tutorial_plotGraph

---
+++ Research/lab/tutorial_plotGraph.py
@@ -0,0 +1,11 @@
+import matplotlib.pyplot as plt
+import  numpy as np
+
+x = np.arange(0, 11)
+y1 = np.cos(x)-1
+y2 = 2*np.sin(x/3, )
+y3 = (x**2)/20, -2
+plt.plot(x, y1, c='c')
+plt.plot(x, y1, c='m')
+plt.plot(x, y1, c='y')
+plt.show()
