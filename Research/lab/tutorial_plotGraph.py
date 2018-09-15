f1fb0a04917aa4bd44d3c5fa03c0260d070b05a5
Bloodblack <kaimaimoo123@gmail.com>
2018-09-15 14:38:12 +0700

    no message

--- Research/lab/tutorial_plotGraph.py
+++ Research/lab/tutorial_plotGraph.py
@@ -1,10 +1,10 @@
 import matplotlib.pyplot as plt
-import  numpy as np
+import numpy as np
 
 x = np.arange(0, 11)
-y1 = np.cos(x)-1
-y2 = 2*np.sin(x/3, )
-y3 = (x**2)/20, -2
+y1 = np.cos(x) - 1
+y2 = 2 * np.sin(x / 3, )
+y3 = (x ** 2) / 20, -2
 plt.plot(x, y1, c='c')
 plt.plot(x, y1, c='m')
 plt.plot(x, y1, c='y')
